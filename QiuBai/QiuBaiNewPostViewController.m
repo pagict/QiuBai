b89bf65c5ed8bda127f78dedde9ebe15d7e31a05
pagict <pagict@gmail.com>
2016-04-19 17:28:17 +0800

    Add Location Info in NewPost VC
    now you can add or dismiss location info in a new post

--- QiuBai/QiuBaiNewPostViewController.m
+++ QiuBai/QiuBaiNewPostViewController.m
@@ -13,23 +13,27 @@
 @interface QiuBaiNewPostViewController ()<UITextViewDelegate, CLLocationManagerDelegate>
 @property (strong, nonatomic)   IBOutlet    UITextView* textView;
 @property (strong, nonatomic)   IBOutlet    UIToolbar* toolBar;
+@property (strong, nonatomic)   IBOutlet    UIView* locationView;
 @property (strong, nonatomic)   IBOutlet    UIButton* locationButton;
-@property (strong, nonatomic)   IBOutlet    UIActivityIndicatorView *activityIndicator;
+@property (strong, nonatomic)   IBOutlet    UIButton* cancelLocationButton;
 @property (strong, nonatomic)   IBOutlet    AnonymousSwitch *anonymousSwitch;
 
 @property (strong, nonatomic)   CLLocationManager* locationManager;
+@property (strong, nonatomic)   CLPlacemark* placemark;
 @end
 
 @implementation QiuBaiNewPostViewController
 - (instancetype)initWithViewFrame:(CGRect)frame {
     self = [super init];
     if (self) {
+        /***************  Text View **************/
         CGFloat toolbarHeight = 44.0;
         CGRect textViewRect = CGRectMake(frame.origin.x, frame.origin.y,
                                          frame.size.width, frame.size.height - toolbarHeight);
         self.textView = [[UITextView alloc] initWithFrame:textViewRect];
         self.textView.dataDetectorTypes = UIDataDetectorTypeAll;
         self.textView.delegate = self;
+        /***************  Tool Bar **************/
         self.toolBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, frame.size.height - toolbarHeight + frame.origin.y,
                                                                    frame.size.width, toolbarHeight)];
         UIBarButtonItem* photoLibButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"gallery"]
@@ -46,24 +50,39 @@
                                                                        action:@selector(recordVideo:)];
         self.toolBar.items = @[photoLibButton, cameraButton, videoButton];
 
-        CGSize locationButtonSize = CGSizeMake(100, 20);
-        CGRect locationButtonFrame = CGRectZero;
-        locationButtonFrame.origin = CGPointMake(5, textViewRect.size.height -  locationButtonSize.height - 2);
-        locationButtonFrame.size = locationButtonSize;
-        self.locationButton = [[UIButton alloc] initWithFrame:locationButtonFrame];
-        self.locationButton.alpha = 0.7;
-        self.locationButton.backgroundColor = [UIColor lightGrayColor];
-        self.locationButton.layer.cornerRadius = 5.0;
+        /***************  Location View **************/
+        self.locationView = [[UIView alloc] init];
+        self.locationView.layer.masksToBounds = YES;
+        self.locationView.contentMode = UIViewContentModeLeft;
+
+        /***************  location Button **************/
+        self.locationButton = [[UIButton alloc] init];
+        self.locationButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
         self.locationButton.titleLabel.font = [UIFont systemFontOfSize:9];
-        [self.locationButton setTitle:@"Attach location" forState:UIControlStateNormal];
         [self.locationButton addTarget:self
                                 action:@selector(toggleLocationEnabled:)
                       forControlEvents:UIControlEventTouchUpInside];
-        [self.textView addSubview:self.locationButton];
-        self.activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(70, 0, 30, 30)];
-        self.activityIndicator.hidesWhenStopped = YES;
-        [self.locationButton addSubview:self.activityIndicator];
+        /***************  Image View and title in locationButton **************/
+        [self.locationButton setImage:[UIImage imageNamed:@"latitude"] forState:UIControlStateNormal];
+        CGRect latitudeImageFrame = self.locationButton.imageView.frame;
+        latitudeImageFrame.origin.x = 0;
+        self.locationButton.imageView.frame = latitudeImageFrame;
+        [self.locationView addSubview:self.locationButton];
 
+        /***************  cancel Location Button **************/
+        self.cancelLocationButton = [[UIButton alloc] init];
+        [self.cancelLocationButton setImage:[UIImage imageNamed:@"cancel_x"] forState:UIControlStateNormal];
+        [self.cancelLocationButton addTarget:self
+                                      action:@selector(cancelLocation:)
+                            forControlEvents:UIControlEventTouchUpInside];
+
+        [self changeAppearanceAsNotLocated];
+        [self placeButtonsAsNotLocated];
+
+        [self.textView addSubview:self.locationView];
+
+
+        /***************  navigation bar Buttons **************/
         UIBarButtonItem* cancelButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel
                                                                                       target:self
                                                                                       action:@selector(cancelPost:)];
@@ -72,10 +91,11 @@
                                                                       target:self
                                                                       action:@selector(post:)];
         [postButton setTintColor:[UIColor yellowColor]];
+
+        /***************  navigation bar TitleView **************/
         self.anonymousSwitch = [[AnonymousSwitch alloc] initWithFrame:CGRectMake(0, 0, 200, 31)];
         self.navigationItem.titleView = self.anonymousSwitch;
 
-//        self.navigationItem.title = @"new Post";
         self.navigationItem.leftBarButtonItem = cancelButton;
         self.navigationItem.rightBarButtonItem = postButton;
 
@@ -124,8 +144,9 @@
 
     CGRect textViewRect = self.textView.frame;
     CGRect toolbarRect = self.toolBar.frame;
-    CGRect addLocationRect = self.locationButton.frame;
+    CGRect addLocationRect = self.locationView.frame;
     if (CGRectContainsRect([UIScreen mainScreen].bounds, keyboardRect)) {
+//    if (CGRectContainsPoint([UIScreen mainScreen].bounds, keyboardRect.origin)) {
         textViewRect.size.height -= keyboardRect.size.height;
         toolbarRect.origin.y -= keyboardRect.size.height;
         addLocationRect.origin.y -= keyboardRect.size.height;
@@ -137,35 +158,157 @@
                      animations:^{
                          self.textView.frame = textViewRect;
                          self.toolBar.frame = toolbarRect;
-                         self.locationButton.frame = addLocationRect;
+                         self.locationView.frame = addLocationRect;
                      }];
 
 }
 
 - (IBAction)toggleLocationEnabled:(id)sender {
-    BOOL isEnabledLocation = [CLLocationManager locationServicesEnabled];
-    self.locationManager = [[CLLocationManager alloc] init];
-    [self.locationManager requestWhenInUseAuthorization];
-    self.locationManager.delegate = self;
-    self.locationManager.distanceFilter = CLLocationDistanceMax;
-    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
+    switch (status) {
+        case kCLAuthorizationStatusDenied: {
+            UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"打开定位"
+                                                                           message:@"请在[设置]-[隐私]-[定位] 中将定位打开"
+                                                                    preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction* dismissAction = [UIAlertAction actionWithTitle:@"confirm"
+                                                                    style:UIAlertActionStyleCancel
+                                                                  handler:^(UIAlertAction * _Nonnull action) {
+
+                                                                  }];
+            [alert addAction:dismissAction];
+
+            [self presentViewController:alert animated:YES completion:nil];
+            return;
+        }
+            break;
+        case kCLAuthorizationStatusNotDetermined:
+            [self.locationManager requestWhenInUseAuthorization];
+            break;
+        default:
+            [self startUpdateLocation];
+            break;
+    }
+}
+
+- (void)     locationManager:(CLLocationManager *)manager
+didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if (status != kCLAuthorizationStatusNotDetermined && status != kCLAuthorizationStatusDenied) {
+        [self startUpdateLocation];
+    }
+}
+
+- (void)startUpdateLocation {
+    [self.locationButton setTitle:@"定位中..." forState:UIControlStateNormal];
+    [self.locationButton sizeToFit];
+    CGRect locationViewFrame = self.locationView.frame;
+    locationViewFrame.size.width = self.locationButton.frame.size.width;
+    self.locationView.frame = locationViewFrame;
     [self.locationManager startUpdatingLocation];
-    [self.activityIndicator startAnimating];
     [self.locationButton setNeedsDisplay];
+}
+
+- (CLLocationManager*)locationManager {
+    if (!_locationManager) {
+        _locationManager = [[CLLocationManager alloc] init];
+        _locationManager.delegate = self;
+        _locationManager.distanceFilter = CLLocationDistanceMax;
+        _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    }
+    return _locationManager;
 }
 
 - (void)locationManager:(CLLocationManager *)manager
      didUpdateLocations:(NSArray<CLLocation *> *)locations {
+
     CLGeocoder* geocoder = [[CLGeocoder alloc] init];
     CLLocation* location = [locations lastObject];
     [geocoder reverseGeocodeLocation:location
-completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
-    
-        CLPlacemark* placemark = [placemarks lastObject];
-        [self.locationButton setTitle:placemark.name forState:UIControlStateNormal];
-        self.locationButton.backgroundColor = [UIColor blueColor];
-        [self.locationButton sizeToFit];
-        [self.activityIndicator stopAnimating];
-}];
+                   completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+
+                       CLPlacemark* placemark = [placemarks lastObject];
+                       if (!placemark) {
+                           return;
+                       }
+                       NSString* location = [NSString stringWithFormat:@"%@ - %@", placemark.locality, placemark.subLocality];
+                       [self changeAppearanceAsLocated:location];
+                       [self placeButtonsAsLocated];
+
+                       self.placemark = placemark;
+                       [self.locationManager stopUpdatingLocation];
+                   }];
+}
+
+- (void)placeButtonsAsLocated {
+    [self.locationButton sizeToFit];
+    self.locationButton.enabled = NO;
+
+    self.cancelLocationButton.hidden = NO;
+    CGRect cancelButtonRect = self.cancelLocationButton.frame;
+    cancelButtonRect.origin.x = self.locationButton.frame.size.width + 3;
+    self.cancelLocationButton.frame = cancelButtonRect;
+    self.cancelLocationButton.enabled = YES;
+
+    CGRect locationViewRect = self.locationView.frame;
+    locationViewRect.size.width = cancelButtonRect.origin.x + cancelButtonRect.size.width + 3;
+    self.locationView.frame = locationViewRect;
+    [self.locationView addSubview:self.cancelLocationButton];
+    [self.locationView bringSubviewToFront:self.cancelLocationButton];
+    [self.locationView setNeedsDisplay];
+}
+
+- (void)changeAppearanceAsLocated:(NSString*)locationString {
+    [self.locationButton setTitle:locationString forState:UIControlStateNormal];
+    self.locationButton.alpha = 1.0;
+    self.locationButton.backgroundColor = [UIColor colorWithRed:0.4
+                                                          green:0.46
+                                                           blue:1.0
+                                                          alpha:1.0];
+
+    self.cancelLocationButton.backgroundColor = self.locationButton.backgroundColor;
+
+
+    self.locationView.backgroundColor = self.cancelLocationButton.backgroundColor;
+}
+
+- (void)placeButtonsAsNotLocated {
+    CGRect locationViewFrame = CGRectZero;
+    locationViewFrame.size = CGSizeMake(120, 20);
+    locationViewFrame.origin = CGPointMake(5, self.textView.frame.size.height -  locationViewFrame.size.height - 18);
+    self.locationView.frame = locationViewFrame;
+    self.locationView.layer.cornerRadius = self.locationView.frame.size.height / 2;
+
+    CGRect locationButtonFrame = locationViewFrame;
+    locationButtonFrame.origin = CGPointMake(0, 0);
+    locationButtonFrame.size.width -= 20;
+    self.locationButton.frame = locationButtonFrame;
+
+    /********  set location View width ********/
+    locationViewFrame.size.width = locationButtonFrame.size.width;
+    self.locationView.frame = locationViewFrame;
+
+    CGRect cancelLocationButtonFrame = locationButtonFrame;
+    cancelLocationButtonFrame.size.width = 15;
+    cancelLocationButtonFrame.size.height = 15;
+    cancelLocationButtonFrame.origin.x = locationButtonFrame.size.width + 6;
+    cancelLocationButtonFrame.origin.y = locationViewFrame.size.height / 2 - cancelLocationButtonFrame.size.height / 2;
+    self.cancelLocationButton.frame = cancelLocationButtonFrame;
+    self.cancelLocationButton.hidden = YES;
+}
+
+- (void)changeAppearanceAsNotLocated {
+    [self.locationButton setTitle:@"添加地理位置信息" forState:UIControlStateNormal];
+    self.locationButton.backgroundColor = [UIColor lightGrayColor];
+    self.locationButton.alpha = 0.7;
+    self.locationButton.enabled = YES;
+
+    self.locationView.backgroundColor = [UIColor clearColor];
+}
+
+- (IBAction)cancelLocation:(id)sender {
+
+    self.placemark = nil;
+    [self.cancelLocationButton removeFromSuperview];
+    [self changeAppearanceAsNotLocated];
+    [self placeButtonsAsNotLocated];
 }
 @end
